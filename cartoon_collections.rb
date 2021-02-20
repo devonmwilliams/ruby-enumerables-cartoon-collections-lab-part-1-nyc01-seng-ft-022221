41891b9e485f87d318b5dd9f677de281290b21d8
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-20 09:00:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def greet_characters(array)
     puts "Hello #{array}"
   end
 end
-greet_characters(array)
+array
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
